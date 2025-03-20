f920a445952ffb558f9ec0abd65e51c675eda6d4
Felix Htoo <felixhtoo@FHs-MacPro-M3.local>
2025-03-21 03:21:59 +0700

    feat: Added Expr 01 for React Three Fiber experiment

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import ColorQuiz from './Colors/index.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import ColorQuiz from "./Colors/index.tsx";
+import { Expr01 } from "./Expr01/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/color-quiz" element={<ColorQuiz />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/color-quiz" element={<ColorQuiz />} />
+        <Route path="/expr-01" element={<Expr01 />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        // primary: colors.indigo,
+      },
+    },
+  },
 //   safelist: [{ pattern: /^bg-/ }, { pattern: /^text-/ }],
   plugins: [],
 };
